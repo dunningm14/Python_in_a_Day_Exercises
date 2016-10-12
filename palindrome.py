83a10d1816e4776650bdf9af3802cad1d63aa12f
dunningm14 <dunningm14@georgefox.edu>
2016-10-11 23:37:14 -0500

    the palindrome code from chapter six

---
+++ palindrome.py
@@ -0,0 +1,12 @@
+# Python 2.7.12
+#
+# Author: Madison Dunning
+#
+# Purpose:  The palendrome exercise from chapter 6
+
+def first(word): 
+    return word[0] 
+def last(word): 
+    return word[-1] 
+def middle(word): 
+    return word[1:-1]
